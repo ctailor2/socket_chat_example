05b45f1ebb5a3ec10a2715c90b876416c6e1ee3d
Chirag Tailor <ctailor2@gmail.com>
2015-06-16 23:35:07 -0500

    Nicknames working with connect/disconnect room messages, but needs refactoring

--- index.js
+++ index.js
@@ -6,15 +6,31 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  io.emit('room message', 'a user connected');
+clients = []
+
+io.on('connect', function(socket){
+  socket.emit('join chat');
+
+  socket.on('enter username', function(username){
+    io.emit('room message', username + ' connected');
+    client = {
+      id: socket.id,
+      username: username
+    };
+    clients.push(client);
+  });
 
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 
   socket.on('disconnect', function(){
-    io.emit('room message', 'user disconnected');
+    client = clients.filter(function(client){
+      return client.id == socket.id;
+    })[0];
+    clientIndex = clients.indexOf(client);
+    io.emit('room message', client.username + ' disconnected');
+    clients.splice(clientIndex, 1);
   });
 });
 
